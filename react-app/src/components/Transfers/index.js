ec0516a2c7f864f591a1a8a18459913c30182727
JuvenalBurguillos <juvie3@gmail.com>
2023-09-12 01:42:05 -0400

    completed transfers with css

--- react-app/src/components/Transfers/index.js
+++ react-app/src/components/Transfers/index.js
@@ -26,6 +26,25 @@ export const Transfers = () => {
             currency: "USD",
       });
 
+      let today = new Date()
+      let dateToday = today.getDate()
+      let numMonthToday = today.getMonth()
+      let monthToday
+
+      if (numMonthToday === 0) monthToday = 'Jan'
+      if (numMonthToday === 1) monthToday = 'Feb'
+      if (numMonthToday === 2) monthToday = 'Mar'
+      if (numMonthToday === 3) monthToday = 'Apr'
+      if (numMonthToday === 4) monthToday = 'May'
+      if (numMonthToday === 5) monthToday = 'Jun'
+      if (numMonthToday === 6) monthToday = 'Jul'
+      if (numMonthToday === 7) monthToday = 'Aug'
+      if (numMonthToday === 8) monthToday = 'Sep'
+      if (numMonthToday === 9) monthToday = 'Oct'
+      if (numMonthToday === 10) monthToday = 'Nov'
+      if (numMonthToday === 11) monthToday = 'Dec'
+
+
       const initialize = async (e) => {
             e.preventDefault();
 
@@ -66,6 +85,9 @@ export const Transfers = () => {
       const account = acctArr.find((account) => account.id == accountId)
       const transferArr = transferArray.filter((transfer) => transfer.accountId == accountId)
 
+      const pendingArr = transferArr.filter((transfer) => !transfer.date_paid)
+      const sentArr = transferArr.filter((transfer) => transfer.date_paid)
+
       const goDetails = () => {
             history.replace(`/accounts/${account.id}`)
       }
@@ -105,53 +127,175 @@ export const Transfers = () => {
             </div>
 
             <div id='inner-transfers-entire-page-2'>
-                  <div id="transfer-div-transfer-page">
-                        <div id='acct-number-transfers'>{`${account.accountType} Account #${account.id}`}</div>
-                        <div id='transfer-text-transfers-page'>Money Transfers</div>
+                  <div id='outer-div-white-upper-boxshadow-transfers-page'>
+                        <div id='white-upper-boxshadow-transfers-page'>
+                              <div id='acct-number-transfers'>{`${account.accountType} Account #${account.id}`}</div>
+                              <div id='transfer-text-transfers-page'>Money Transfers</div>
 
-                        <div id="main-transfer-bar">
+                              <div id="main-transfer-bar">
 
-                        <form id="main-transfer-form" onSubmit={initialize}>
+                              <form id="main-transfer-form" onSubmit={initialize}>
 
-                              <label>
-                                    <input id='transfer-payee-input'
-                                    type="text"
-                                    placeholder="Who are you sending money to?"
-                                    value={payee}
-                                    onChange={(e) => setPayee(e.target.value)}
-                                    maxLength="200"
-                                    required
-                                    />
-                              </label>
-                              <label>
-                                    <input id='transfer-amount-input'
-                                    type="number"
-                                    step="0.01"
-                                    placeholder="How much do you want to send?"
-                                    value={amount}
-                                    onChange={(e) => setAmount(e.target.value)}
-                                    max={account.funds}
-                                    required
-                                    />
-                              </label>
-                              <button className="grow pointer" type="submit">Start Transfer</button>
+                                    <label>
+                                          <input id='transfer-payee-input'
+                                          type="text"
+                                          placeholder="Who are you sending money to?"
+                                          value={payee}
+                                          onChange={(e) => setPayee(e.target.value)}
+                                          maxLength="200"
+                                          required
+                                          />
+                                    </label>
+                                    <label>
+                                          <input id='transfer-amount-input'
+                                          type="number"
+                                          step="0.01"
+                                          placeholder="How much do you want to send?"
+                                          value={amount}
+                                          onChange={(e) => setAmount(e.target.value)}
+                                          max={account.funds}
+                                          required
+                                          />
+                                    </label>
+                                    <button className="grow pointer" type="submit">Pend Transfer</button>
 
 
 
-                              </form>
+                                    </form>
+
+
+                              </div>
 
 
                         </div>
+
+
+
+                  </div>
+
+                        <div id="transfer-div-transfer-page">
 
 
                         <div id='pending-text-transfers-page'>Pending Transfers</div>
 
                         <div id='pending-transfer-category-bar'>
-                              <div>DATE</div>
+                              <div id='date-transfers-page'>DATE</div>
                               <div>MONEY RECEIVER</div>
                               <div>STATUS</div>
                               <div>AMOUNT</div>
                         </div>
+
+                        {
+                              !pendingArr.length ?
+                              <div id='pending-transfer-body-empty'>
+                                    <h3>No Pending Transfers</h3>
+
+                              </div>
+
+                              :
+                              pendingArr.map((transfer) => (
+                                    <div id='pending-transfer-body'>
+                                          <div>
+                                          <div id='month-today-transfer-page'>
+                                          {monthToday}
+                                          </div>
+                                          <div id='date-today-transfers-page'>
+                                          {dateToday}
+                                          </div>
+                                          </div>
+                                          <div id='payee-transfers-page'>{transfer.payee}</div>
+                                          <div>
+                                          {
+                                          transfer.amount <= account.funds ?
+
+
+                                                transfer.date_paid ?
+                                                <div>
+                                                {`Sent on ${transfer.date_paid}`}
+                                                </div>
+                                                :
+
+                                                <div id='send-butt-transfers-page' className="grow pointer" onClick={()=>send(transfer.id)} >
+                                                Send Money
+                                                </div>
+
+
+
+                                          :
+
+
+                                                transfer.date_paid ?
+                                                <div>
+                                                {`Sent on ${transfer.date_paid}`}
+                                                </div>
+                                                :
+                                                <div>
+                                                Not Enough Funds
+                                                </div>
+
+
+
+                                          }
+
+                                          <div id='delete-butt-transfers-page' className="grow pointer" onClick={()=>deleteTransfer(transfer.id)} >Delete</div>
+
+
+                                          </div>
+                                          <div id='amount-transfers-page'>{dollar.format(transfer.amount)}</div>
+                                    </div>
+
+
+                              ))
+                        }
+
+
+
+                        <div id='pending-text-transfers-page'>Past Transfers</div>
+
+                        <div id='pending-transfer-category-bar'>
+                              <div id='date-transfers-page'>DATE</div>
+                              <div>MONEY RECEIVER</div>
+                              <div>STATUS</div>
+                              <div>AMOUNT</div>
+                        </div>
+
+                        {
+                              !sentArr.length ?
+                              <div id='pending-transfer-body-empty'>
+                                    <h3>No Past Transfers</h3>
+
+                              </div>
+
+                              :
+                              sentArr.map((transfer) => (
+                                    <div id='pending-transfer-body'>
+                                          <div>
+                                          <div id='month-today-transfer-page'>
+                                          {monthToday}
+                                          </div>
+                                          <div id='date-today-transfers-page'>
+                                          {transfer.date_paid.slice(5,7)}
+                                          </div>
+                                          </div>
+                                          <div id='payee-transfers-page'>{transfer.payee}</div>
+                                          <div>
+                                          <div>
+                                                {`Sent ${transfer.date_paid.slice(0,16)}`}
+                                          </div>
+
+
+                                          </div>
+
+
+
+
+
+                                          <div id='amount-transfers-page'>{dollar.format(transfer.amount)}</div>
+                                    </div>
+
+
+                              ))
+                        }
 
 
 
@@ -162,6 +306,15 @@ export const Transfers = () => {
 
 
 
+
+
+
+
+
+
+
+
+{/*
                   {
                         transferArr.length ?
                         transferArr.map((transfer) => (
@@ -221,7 +374,7 @@ export const Transfers = () => {
                         )) : null
 
 
-                  }
+                  } */}
 
 
 
